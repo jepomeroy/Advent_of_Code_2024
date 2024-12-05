b0205afdf1f740d613fe7296b644747fcde091d6
Jon Pomeroy <earl.pomeroy42@gmail.com>
2024-12-04 21:39:12 -0700

    chore: staging some files for today

---
+++ day5/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// First element in os.Args is always the program name,
+	// So we need at least 2 arguments to have a file name argument.
+	if len(os.Args) < 2 {
+		fmt.Println("Missing parameter, provide file name!")
+		return
+	}
+
+	data, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		check(err, "Error reading file")
+	}
+
+	// part1(string(data))
+	part2(string(data))
+}
+
+func part1(data string) {
+}
+
+func part2(data string) {
+}
+
+func check(err error, msg string) {
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println(msg)
+		os.Exit(1)
+	}
+}
