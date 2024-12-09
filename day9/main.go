12cbce6e90c7de526017dd0fe03accfd5ee338af
Jon Pomeroy <earl.pomeroy42@gmail.com>
2024-12-09 12:45:51 -0700

    fix: day9, part 1 complete

--- day9/main.go
+++ day9/main.go
@@ -3,7 +3,76 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
+
+func makeDiskBlocks(length int, block int) []int {
+	blocks := []int{}
+
+	// fmt.Printf("length: %d, block: %c\n", length, block)
+
+	for i := 0; i < length; i++ {
+		blocks = append(blocks, block)
+	}
+
+	return blocks
+}
+
+func expandDiskMap(diskMap []int) []int {
+	expandedMap := []int{}
+	id := 0
+	createBlocks := true
+	var block int
+
+	for _, v := range diskMap {
+		if createBlocks {
+			block = id
+			id++
+		} else {
+			block = -1
+		}
+		expandedMap = append(expandedMap, makeDiskBlocks(v, block)...)
+		createBlocks = !createBlocks
+	}
+
+	// fmt.Println(string(expandedMap))
+	return expandedMap
+}
+
+func defragDiskMap(diskMap *[]int) {
+	i, j := 0, len(*diskMap)-1
+
+	for i < j {
+		if (*diskMap)[i] != -1 {
+			i++
+		} else if (*diskMap)[j] == -1 {
+			j--
+		} else {
+			// fmt.Println(string(*diskMap))
+			(*diskMap)[i], (*diskMap)[j] = (*diskMap)[j], (*diskMap)[i]
+			i++
+			j--
+		}
+	}
+
+	// fmt.Println(string(*diskMap))
+}
+
+func calculateCheckSum(diskMap []int) int {
+	checksum := 0
+
+	for i, v := range diskMap {
+		if v == -1 {
+			break
+		}
+
+		checksum += (v * i)
+
+		// fmt.Printf("i: %d, d: %d, checksum: %d\n", i, d, checksum)
+	}
+
+	return checksum
+}
 
 func main() {
 	// First element in os.Args is always the program name,
@@ -18,19 +87,39 @@ func main() {
 		check(err, "Error reading file")
 	}
 
-	// part1(string(data))
-	part2(string(data))
+	diskMap := []int{}
+	for _, v := range data {
+		if v == '\n' {
+			continue
+		}
+
+		d, err := strconv.Atoi(string(v))
+		if err != nil {
+			check(err, "Error converting to int")
+		}
+		diskMap = append(diskMap, d)
+	}
+
+	part1(diskMap)
+	// part2(diskMap)
 }
 
-func part1(data string) {
+func part1(diskMap []int) {
 	value := 0
+
+	expandedMap := expandDiskMap(diskMap)
+
+	defragDiskMap(&expandedMap)
+
+	value = calculateCheckSum(expandedMap)
 
 	fmt.Println(value)
 }
 
-func part2(data string) {
+func part2(diskMap []int) {
 	value := 0
 
+	fmt.Println(diskMap)
 	fmt.Println(value)
 }
 
